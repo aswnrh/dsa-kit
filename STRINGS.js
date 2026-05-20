dde1911b4f6720155dd23156482c42a028f8f581
Aswin Ramesh <aswnrh@Aswins-MacBook-Air.local>
2026-05-20 23:55:09 +0530

    feat: add isPalindrome function to STRINGS.js utility library

--- STRINGS.js
+++ STRINGS.js
@@ -16,3 +16,21 @@ const revStr = (s) => {
 }
 
 // console.log(revStr(str))
+
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(isPalindrome("madam"));
